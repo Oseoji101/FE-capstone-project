18fc8c6ca86be13cc5ee1b9bcd44ce9c2bcfda2e
oseoji101 <nonso.kelvin8770@gmail.com>
2025-10-12 23:25:03 +0100

    updating still ongoing

--- capstone-movie-database/src/App.jsx
+++ capstone-movie-database/src/App.jsx
@@ -2,19 +2,27 @@ import { useState } from 'react'
 import './App.css'
 import MovieList from './components/movie-list'
 import SearchBar from './components/SearchBar'
-
+import MovieDetails from './components/MovieDetails'
+import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
+import MovieCard from './components/MovieCard'
 function App() {
   
 
   return (
     <>
       <div>
-        <MovieList />
         <SearchBar />
+      
+        <Router>
+          <Routes>
+            <Route path='/' element={<MovieList />}></Route>
+            <Route path='/movies/:id' element={<MovieDetails/>}></Route>
+          </Routes>
+        </Router>    
         
       </div>
     </>
   )
 }
 
-export default App
+export default App;
