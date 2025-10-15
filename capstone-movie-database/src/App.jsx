23957fb1a88640d61b8d1bca889852a0a16c1cdc
oseoji101 <nonso.kelvin8770@gmail.com>
2025-10-15 22:32:01 +0100

    tailwind design of App.jsx

--- capstone-movie-database/src/App.jsx
+++ capstone-movie-database/src/App.jsx
@@ -6,15 +6,15 @@ import MovieDetails from './components/MovieDetails'
 import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState('batman');
+  const [searchTerm, setSearchTerm] = useState('');
   
 
   return (
     <>
       <div>
       
-        <Router>
-          <Routes>
+        <Router className="min-h-screen bg-gray-600 text-gray-900">
+          <Routes className="max-w-7xl mx-auto p-5">
             <Route path='/' element={
               <>
                 <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm}/>
